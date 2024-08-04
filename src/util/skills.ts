c975d8c0982b4fe100af2384f89ee7a4fb5d4080
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-08-04 13:17:20 -0500

    Creación del componente HeroAnimation

---
+++ src/util/skills.ts
@@ -0,0 +1,16 @@
+const SKILLS = [
+    { slug: "java", lalbel: "Java" },
+    { slug: "spring", lalbel: "Spring Boot"},
+    { slug: "cs", lalbel: "C#" },
+    { slug: "dotnet", lalbel: ".Net" },
+    { slug: "flask", lalbel: "Flask" },
+    { slug: "html", lalbel: "Html" },
+    { slug: "css", lalbel: "Css" },
+    { slug: "js", lalbel: "JavaScript" },
+    { slug: "react", lalbel: "React" },
+    { slug: "tailwind", lalbel: "Tailwind" },
+    { slug: "docker", lalbel: "Docker" },
+    { slug: "postgres", lalbel: "Postgres" },
+  ];
+
+  export default SKILLS;
